16a63f254db3e8b3af881b1f8f2ea11c3d46fbd7
robbie <r.heygate@gmail.com>
2016-07-09 11:02:25 +0100

    Search complete

--- app/controller.js
+++ app/controller.js
@@ -17,15 +17,22 @@
             format: "json",
             jsoncallback: "JSON_CALLBACK"
         }
+
+        $scope.disableFilter = '';
         $scope.items = [];
-				$scope.post =  $location.hash();
+				$scope.tags = [];
+				$scope.post = $location.hash();
 				console.log($scope.post);
+				console.log($scope.tags);
         FlickrData.query('JSONP', '/services/feeds/photos_public.gne', request, {})
             .then(function(obj) {
                 $scope.obj = obj.data;
                 console.log($scope.obj);
                 angular.forEach($scope.obj.items, function(value) {
+										value.tagString = value.tags;
+                    $scope.tags.push(value.tags.split(" "));
                     value.tags = value.tags.split(" ");
+
                     this.push(value);
                 }, $scope.items);
 
